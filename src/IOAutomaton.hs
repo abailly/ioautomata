8e380ee46ebac04ed81749177e81d944d7b30e5b
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-05-03 06:12:51 +0200

    remove dependency on graphviz and bump stack version

--- src/IOAutomaton.hs
+++ src/IOAutomaton.hs
@@ -1,12 +1,10 @@
 module IOAutomaton
   ( module IOAutomaton.IOAutomaton
   , module IOAutomaton.Generator
-  , module IOAutomaton.Graph
   , module IOAutomaton.Model
   )
        where
 
 import           IOAutomaton.Generator
-import           IOAutomaton.Graph
 import           IOAutomaton.IOAutomaton
 import           IOAutomaton.Model
